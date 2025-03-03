17b512802c6d3aa11fe934372e48f4dcc9f8c514
fahryafc <fahry.afc@gmail.com>
2025-03-03 11:55:10 +0700

    cuma ubah tittle

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Konrix - Responsive Tailwind Admin Dashboard</title>
+    <title>Finre - Finre.id</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
     <meta content="coderthemes" name="author">
