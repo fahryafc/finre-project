a1f918eed6b3940658eb6d1b6f8fc1b9d71e9dba
fahryafc <fahry.afc@gmail.com>
2025-03-08 15:50:12 +0700

    fixed web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\InvitesController;
 use App\Http\Controllers\PagesController;
