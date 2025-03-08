21c48c6cf7eb87ddde63c6b572653e8fb2c53b59
Rahmat Fahrezi S <fahry.afc@gmail.com>
2025-03-09 04:52:44 +0700

    fix trial and icon sidebar

--- resources/views/layouts/shared/sidebar.blade.php
+++ resources/views/layouts/shared/sidebar.blade.php
@@ -1,9 +1,17 @@
 <div class="app-menu">
 
     <!-- Sidenav Brand Logo -->
-    <a href="any', 'index') }}" class="logo-box">
-        <div>
-            <img src="{{ asset('images/brands/logo.png') }}" class="logo-lg h-10" alt="Small logo">
+    <a href="javascript:void(0)" class="logo-box">
+        <!-- Light Brand Logo -->
+        <div class="logo-light">
+            <img src="/images/finre.png" class="logo-lg h-6" alt="Light logo">
+            <img src="/images/finre-mini.png" class="logo-sm" alt="Small logo">
+        </div>
+
+        <!-- Dark Brand Logo -->
+        <div class="logo-dark">
+            <img src="/images/finre.png" class="logo-lg h-6" alt="Dark logo">
+            <img src="/images/finre-mini.png" class="logo-sm" alt="Small logo">
         </div>
     </a>
 
@@ -16,7 +24,7 @@
     <!--- Menu -->
     <div class="srcollbar" data-simplebar>
         <ul class="menu" data-fc-type="accordion">
-            <li class="menu-title">Menu</li>
+            <!-- <li class="menu-title">Menu</li> -->
 
             @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('dashboard') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
                 <li class="menu-item">
@@ -25,7 +33,7 @@
                         <span class="menu-text"> Dashboard </span>
                     </a>
                 </li>
-            @endif
+                @endif
 
             @if (auth()->user()->hasRole('owner'))
                 <li class="menu-item">
@@ -51,7 +59,7 @@
             @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('penjualan') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
                 <li class="menu-item">
                     <a href="/penjualan" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
+                        <span class="menu-icon"><i class="ti ti-report-money"></i></span>
                         <span class="menu-text"> Penjualan </span>
                     </a>
                 </li>
@@ -60,7 +68,7 @@
             @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('pengeluaran') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
                 <li class="menu-item">
                     <a href="/pengeluaran" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
+                        <span class="menu-icon"><i class="ti ti-shopping-cart"></i></span>
                         <span class="menu-text"> Pengeluaran </span>
                     </a>
                 </li>
@@ -68,8 +76,8 @@
 
             @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('hutang-piutang') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
                 <li class="menu-item">
-                    <a href="/hutang-piutang" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
+                    <a href="/hutangpiutang" class="menu-link">
+                        <span class="menu-icon"><i class="ti ti-chart-histogram"></i></span>
                         <span class="menu-text"> Hutang & Piutang </span>
                     </a>
                 </li>
@@ -77,116 +85,165 @@
 
             @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('kas-bank') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
                 <li class="menu-item">
-                    <a href="/kas-bank" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
+                    <a href="/kasdanbank" class="menu-link">
+                        <span class="menu-icon"><i class="ti ti-wallet"></i></span>
                         <span class="menu-text"> Kas & Bank </span>
                     </a>
                 </li>
             @endif
 
             @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('pajak') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
-                <li class="menu-item">
-                    <a href="/pajak" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
-                        <span class="menu-text"> Pajak </span>
-                    </a>
-                </li>
-            @endif
-
-            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('produk-inventori') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
-                <li class="menu-item">
-                    <a href="/produk-inventori" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
-                        <span class="menu-text"> Produk & Inventori </span>
-                    </a>
-                </li>
-            @endif
-
-            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('aset') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
-                <li class="menu-item">
-                    <a href="/aset" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
-                        <span class="menu-text"> Aset </span>
-                    </a>
-                </li>
-            @endif
-
-            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('laporan') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
-                <li class="menu-item">
-                    <a href="/laporan" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
-                        <span class="menu-text"> Laporan </span>
-                    </a>
-                </li>
-            @endif
-
-            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('kontak') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
-                <li class="menu-item">
-                    <a href="/kontak" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
-                        <span class="menu-text"> Kontak </span>
-                    </a>
-                </li>
-            @endif
-
-            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('akun') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
-                <li class="menu-item">
-                    <a href="/akun" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
-                        <span class="menu-text"> Akun </span>
-                    </a>
-                </li>
-            @endif
-
-            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('modal') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
-                <li class="menu-item">
-                    <a href="/modal" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
-                        <span class="menu-text"> Modal </span>
-                    </a>
-                </li>
-            @endif
-
-            @if (auth()->user()->hasRole('inviter'))
-                <li class="menu-item">
-                    <a href="/paket-setting" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_box_3_line"></i></span>
-                        <span class="menu-text"> Paket </span>
-                    </a>
-                </li>
-            @elseif(\App\Models\Invites::where('email', auth()->user()->email)->where('status', '!=', 'accepted')->exists() || !\App\Models\Invites::where('email', auth()->user()->email)->exists() && !auth()->user()->hasRole('owner'))
-                <li class="menu-item">
-                    <a href="/daftar-paket" class="menu-link">
-                        <span class="menu-icon"><i class="mgc_box_3_line"></i></span>
-                        <span class="menu-text"> Daftar Paket </span>
-                    </a>
-                </li>
-            @endif
             <li class="menu-item">
-                <a href="javascript:void(0)" data-fc-type="collapse" class="menu-link">
-                    <span class="menu-icon"><i class="mgc_building_2_line"></i></span>
-                    <span class="menu-text"> Project </span>
+                <a href="javascript:void(0)" data-fc-type="collapse" class="menu-link fc-collapse">
+                    <span class="menu-icon"><i class="ti ti-receipt-tax"></i></span>
+                    <span class="menu-text"> Pajak </span>
                     <span class="menu-arrow"></span>
                 </a>
 
-                <ul class="sub-menu hidden">
+                <ul class="sub-menu hidden" style="">
                     <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">List</span>
+                        <a href="{{ route('pajak.ppn') }}" class="menu-link">
+                            <span class="menu-text">PPN</span>
                         </a>
                     </li>
                     <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Detail</span>
+                        <a href="{{ route('pajak.pph') }}" class="menu-link">
+                            <span class="menu-text">PPH</span>
                         </a>
                     </li>
                     <li class="menu-item">
-                        <a href="#" class="menu-link">
-                            <span class="menu-text">Create</span>
+                        <a href="{{ route('pajak.ppnbm') }}" class="menu-link">
+                            <span class="menu-text">PPnBM</span>
                         </a>
                     </li>
                 </ul>
             </li>
+            @endif
+
+            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('produk-inventori') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
+            <li class="menu-item">
+                <a href="{{ route('produkdaninventori.index') }}" class="menu-link">
+                    <span class="menu-icon"><i class="ti ti-building-store"></i></span>
+                    <span class="menu-text"> Produk & Inventori </span>
+                </a>
+            </li>
+            @endif
+
+            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('aset') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
+            <li class="menu-item">
+                <a href="javascript:void(0)" data-fc-type="collapse" class="menu-link fc-collapse">
+                    <span class="menu-icon"><i class="ti ti-database-cog"></i></span>
+                    <span class="menu-text"> Asset </span>
+                    <span class="menu-arrow"></span>
+                </a>
+
+                <ul class="sub-menu hidden" style="">
+                    <li class="menu-item">
+                        <a href="{{ route('asset.asset_tersedia') }}" class="menu-link">
+                            <span class="menu-text">Asset Tersedia</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('asset.asset_terjual') }}" class="menu-link">
+                            <span class="menu-text">Asset Terjual</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            @endif
+
+            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('laporan') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
+            <li class="menu-item">
+                <a href="javascript:void(0)" data-fc-type="collapse" class="menu-link fc-collapse">
+                    <span class="menu-icon"><i class="mgc_chart_bar_line"></i></span>
+                    <span class="menu-text"> Laporan </span>
+                    <span class="menu-arrow"></span>
+                </a>
+
+                <ul class="sub-menu hidden" style="">
+                    <li class="menu-item">
+                        <a href="{{ route('jurnal.index') }}" class="menu-link">
+                            <span class="menu-text">Jurnal</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('aruskas.index') }}" class="menu-link">
+                            <span class="menu-text">Arus Kas</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('neraca.index') }}" class="menu-link">
+                            <span class="menu-text">Neraca</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('labarugi.index') }}" class="menu-link">
+                            <span class="menu-text">Laba Rugi</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            @endif
+
+            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('kontak') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
+            <li class="menu-item">
+                <a href="{{ route('kontak.index') }}" class="menu-link">
+                    <span class="menu-icon"><i class="ti ti-address-book"></i></span>
+                    <span class="menu-text"> Kontak </span>
+                </a>
+            </li>
+            @endif
+
+            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('akun') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
+            <li class="menu-item">
+                <a href="{{ route('akun.index') }}" class="menu-link">
+                    <span class="menu-icon"><i class="ti ti-chart-infographic"></i></span>
+                    <span class="menu-text"> Akun </span>
+                </a>
+            </li>
+            @endif
+
+            @if (auth()->user()->hasRole('inviter') || \App\Models\Subscription::where('user_id', auth()->user()->id)->where('nama_paket', 'Free Trial')->where('status', 'active')->exists() || auth()->user()->can('modal') && \App\Models\Invites::where('email', auth()->user()->email)->where('status', 'accepted')->exists())
+            <li class="menu-item">
+                <a href="{{ route('modal.index') }}" class="menu-link">
+                    <span class="menu-icon"><i class="ti ti-coins"></i></span>
+                    <span class="menu-text"> Modal </span>
+                </a>
+            </li>
+            @endif
+
+            <li class="menu-item">
+                <a href="javascript:void(0)" data-fc-type="collapse" class="menu-link fc-collapse">
+                    <span class="menu-icon"><i class="ti ti-settings"></i></span>
+                    <span class="menu-text"> Pengaturan </span>
+                    <span class="menu-arrow"></span>
+                </a>
+
+                <ul class="sub-menu hidden" style="">
+                    @if (auth()->user()->hasRole('inviter'))
+                    <li class="menu-item">
+                        <a href="/paket-setting" class="menu-link">
+                            <!-- <span class="menu-icon"><i class="mgc_box_3_line"></i></span> -->
+                            <span class="menu-text"> Paket </span>
+                        </a>
+                    </li>
+                    @elseif(\App\Models\Invites::where('email', auth()->user()->email)->where('status', '!=', 'accepted')->exists() || !\App\Models\Invites::where('email', auth()->user()->email)->exists() && !auth()->user()->hasRole('owner'))
+                    <li class="menu-item">
+                        <a href="/daftar-paket" class="menu-link">
+                            <!-- <span class="menu-icon"><i class="mgc_box_3_line"></i></span> -->
+                            <span class="menu-text"> Daftar Paket </span>
+                        </a>
+                    </li>
+                    @endif
+
+                    <li class="menu-item">
+                        <a href="/pengaturan" class="menu-link">
+                            <span class="menu-text">Setting Profil</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
         </ul>
     </div>
 </div>
